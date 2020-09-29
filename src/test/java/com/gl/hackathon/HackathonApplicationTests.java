95cdd0690bef55d08602800413bdd2701362aced
punith <punithklk79@gmail.com>
2020-09-29 14:46:41 +0530

    Initial commit

---
+++ src/test/java/com/gl/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
